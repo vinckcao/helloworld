d324e9a7156d80748a97cb819134b4785c0d8482
Lauralee Morissette <0512mv@163.com>
2025-07-15 15:01:50 +0000

    添加***

--- program.cs
+++ program.cs
@@ -18,7 +18,7 @@ namespace HelloWorldApp
             {
                 // 显示欢迎信息
                 Console.WriteLine("=================================");
-                Console.WriteLine("    欢迎123使用 Hello World 程序!");
+                Console.WriteLine("    ***欢迎使用 Hello World 程序!***");
                 Console.WriteLine("=================================");
                 Console.WriteLine();
 
